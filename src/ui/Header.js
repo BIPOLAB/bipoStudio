cb05e5bd30180a8c4a62c4ff7275398707ff0f40
BIPOLAB engineering <31290207+BIPOLAB@users.noreply.github.com>
2026-08-28 19:29:50 -0300

    feat: add configuration save controls

--- src/ui/Header.js
+++ src/ui/Header.js
@@ -1,59 +1,60 @@
+import { Events } from "../core/Events.js";
+
 export default class Header {
 
     constructor(element, eventBus) {
-
         this.element = element;
-
         this.eventBus = eventBus;
-
         this.device = null;
+        this.dirty = false;
 
-        this.eventBus.on(
-
-            "session:changed",
-
-            this.onSessionChanged.bind(this)
-
-        );
-
+        this.eventBus.on(Events.SESSION_CHANGED, this.onSessionChanged.bind(this));
+        this.eventBus.on(Events.WORKING_COPY_CHANGED, this.onWorkingCopyChanged.bind(this));
     }
 
     show() {
-
         this.render();
-
     }
 
     onSessionChanged(device) {
-
         this.device = device;
-
         this.render();
+    }
 
+    onWorkingCopyChanged(payload = {}) {
+        this.dirty = Boolean(payload.dirty);
+        this.render();
     }
 
     render() {
-
         this.element.innerHTML = `
-
-            <div class="header-left">
-
-                bipoStudio
-
+            <div class="brand">
+                <span class="brand__name">bipoStudio</span>
+                <span class="brand__device">
+                    ${this.device ? `${this.device.name} · FW ${this.device.firmware}` : "Waiting for device..."}
+                </span>
             </div>
 
-            <div class="header-right">
-
-                ${
-                    this.device
-                        ? `${this.device.name} · FW ${this.device.firmware}`
-                        : "Waiting for device..."
-                }
-
+            <div class="header__actions">
+                <span class="header__dirty ${this.dirty ? "header__dirty--visible" : ""}">
+                    Unsaved changes
+                </span>
+                <button class="button button--secondary" type="button" data-action="reset" ${this.dirty ? "" : "disabled"}>
+                    Reset
+                </button>
+                <button class="button button--primary" type="button" data-action="save" ${this.dirty ? "" : "disabled"}>
+                    Save
+                </button>
             </div>
-
         `;
 
+        this.element.querySelector('[data-action="reset"]')?.addEventListener("click", () => {
+            this.eventBus.emit(Events.WORKING_COPY_CHANGED, { request: "reset" });
+        });
+
+        this.element.querySelector('[data-action="save"]')?.addEventListener("click", () => {
+            this.eventBus.emit(Events.WORKING_COPY_CHANGED, { request: "commit" });
+        });
     }
 
 }
